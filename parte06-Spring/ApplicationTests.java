9a0db8c1ceff21beb58c911a3c101f960c6e7c78
lucasM0201 <lucasmadalaodesousa@gmail.com>
2026-06-08 21:44:53 -0300

    Add files via upload

--- parte06-Spring/ApplicationTests.java
+++ parte06-Spring/ApplicationTests.java
@@ -3,9 +3,9 @@ package com.crud.javalanches.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.crud.javalanches.models.Categoria;
+import com.crud.javalanches.models.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
 }
